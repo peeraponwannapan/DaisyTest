8264ac0b9be3ccec8c5e3a2be4ee2f13633609b7
Manx <manmeisterz@gmail.com>
2024-11-02 05:58:35 +0700

    TEST

--- src/views/components/ProfileCard.vue
+++ src/views/components/ProfileCard.vue
@@ -25,6 +25,7 @@ export default {
         const profileStore = useProfileStore();
         const profile = ref(profileStore.profile);
         onMounted(async () => {
+            await profileStore.fetchProfile()
         });
 
         watch(() => profileStore.profile, (newProfile) => {
