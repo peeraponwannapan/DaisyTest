00e3625b7847f34ece955aec2a8e39c5134cd98b
Manx <manmeisterz@gmail.com>
2024-11-29 00:56:59 +0700

    TEST

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router'
+import { accessTokenStore } from './stores/useAccessTokenLine';
+import { useProfileStore } from './stores/useProfileStore';
+const accessToken = accessTokenStore();
+const profileStore = useProfileStore();
+
+        onMounted(async () => {
+            await profileStore.fetchProfile()
+            await accessToken.getIDToken()
+            if (accessToken.isTokenExpired()) {
+                await accessToken.clearToken()
+            }
+        });
 </script>
 
 <template>
