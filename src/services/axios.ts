b85412ce1afd36feb7d6c1cfe25d3219a62f4c3d
Manx <manmeisterz@gmail.com>
2024-11-29 01:12:36 +0700

    TEST

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,10 +4,12 @@ import axios from 'axios'
 
 export const backEndApi = axios.create({ baseURL: baseAPIURL })
 
-backEndApi.interceptors.request.use((config) => {
+backEndApi.interceptors.request.use(async (config) => {
     const getAccessToken = accessTokenStore();
+    await getAccessToken.getIDToken(); // เรียกฟังก์ชันโหลด token
+    const token = getAccessToken?.accessToken;
     if (getAccessToken) {
-      config.headers.Authorization = `Bearer ${getAccessToken?.accessToken}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   }, (error) => {
