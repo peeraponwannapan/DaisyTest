5ba31ad8f78c80a077feeca27dbbe4888600d48d
Manx <manmeisterz@gmail.com>
2024-11-29 02:26:19 +0700

    TEST

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { RouterView } from 'vue-router'
-import { accessTokenStore } from './stores/useAccessTokenLine';
 import { useProfileStore } from './stores/useProfileStore';
 
 onMounted(async () => {
-  const accessToken = accessTokenStore()
   const profileStore = useProfileStore()
 
   // Fetch profile and ID token before setting up interceptors
   await profileStore.fetchProfile()
-  await accessToken.getIDToken()
-  console.log("Profile and ID Token fetched successfully");
-  console.log("Access Token:", accessToken.accessToken);
+
   // Setup Axios interceptors after initialization
 
 })
