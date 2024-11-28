b04bc59340f7713a7ce9b976cce70763da54701e
Manx <manmeisterz@gmail.com>
2024-11-29 01:25:23 +0700

    TEST

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,16 +1,19 @@
 import { baseAPIURL } from '@/config'
-import { accessTokenStore } from '@/stores/useAccessTokenLine';
+import { accessTokenStore } from '@/stores/useAccessTokenLine'
 import axios from 'axios'
 
 export const backEndApi = axios.create({ baseURL: baseAPIURL })
 
-backEndApi.interceptors.request.use(async (config) => {
-    const getAccessToken = accessTokenStore();
-    const token = getAccessToken?.accessToken;
+backEndApi.interceptors.request.use(
+  config => {
+    const getAccessToken = accessTokenStore()
+    const token = getAccessToken?.accessToken
     if (getAccessToken) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
-    return config;
-  }, (error) => {
-    return Promise.reject(error);
-});
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  },
+)
