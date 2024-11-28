c24d515f630ce91c28dbca46c00aeb9cb135e85c
Manx <manmeisterz@gmail.com>
2024-11-29 01:30:01 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@ import { onMounted } from 'vue';
 import { RouterView } from 'vue-router'
 import { accessTokenStore } from './stores/useAccessTokenLine';
 import { useProfileStore } from './stores/useProfileStore';
-const accessToken = accessTokenStore();
-const profileStore = useProfileStore();
+import setupAxiosInterceptors from './services/axios';
 
 onMounted(async () => {
+  const accessToken = accessTokenStore()
+  const profileStore = useProfileStore()
+
+  // Fetch profile and ID token before setting up interceptors
   await profileStore.fetchProfile()
   await accessToken.getIDToken()
-});
+
+  // Setup Axios interceptors after initialization
+  setupAxiosInterceptors()
+})
 </script>
 
 <template>
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,16 +4,18 @@ import axios from 'axios'
 
 export const backEndApi = axios.create({ baseURL: baseAPIURL })
 
-backEndApi.interceptors.request.use(
-  config => {
-    const getAccessToken = accessTokenStore()
-    const token = getAccessToken?.accessToken
-    if (getAccessToken) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  error => {
-    return Promise.reject(error)
-  },
-)
+export default function setupAxiosInterceptors() {
+  backEndApi.interceptors.request.use(
+    config => {
+      const getAccessToken = accessTokenStore()
+      const token = getAccessToken?.accessToken
+      if (getAccessToken) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    error => {
+      return Promise.reject(error)
+    },
+  )
+}
