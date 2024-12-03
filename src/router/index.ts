1b1f985e1e35ac9f920f2b4ba22b0dd9167a53ca
Manx <manmeisterz@gmail.com>
2024-12-03 22:05:55 +0700

    TEST

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '@/views/ProductView.vue'
 import OrderStatusView from '@/views/order-status/OrderStatusView.vue'
-import { accessTokenStore } from '@/stores/useAccessTokenLine'
+// import { accessTokenStore } from '@/stores/useAccessTokenLine'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,22 +25,22 @@ const router = createRouter({
   ],
 })
 
-router.beforeEach(async (to, from, next) => {
-  const accessToken = accessTokenStore();
-  if (!accessToken.accessToken) {
-    try {
-      await accessToken.getIDToken();
-      console.log("Profile and ID Token fetched successfully");
-      alert("Profile and ID Token fetched successfully")
-    } catch (err) {
-      alert(err)
-      console.error("Error fetching ID Token:", err);
-      next('/error'); // เปลี่ยนเส้นทางไปยังหน้า error
-      return;
-    }
-  }
-  alert(accessToken.accessToken)
-  next(); // อนุญาตให้ไปยังเส้นทางที่ร้องขอ
-});
+// router.beforeEach(async (to, from, next) => {
+//   const accessToken = accessTokenStore();
+//   if (!accessToken.accessToken) {
+//     try {
+//       await accessToken.getIDToken();
+//       console.log("Profile and ID Token fetched successfully");
+//       alert("Profile and ID Token fetched successfully")
+//     } catch (err) {
+//       alert(err)
+//       console.error("Error fetching ID Token:", err);
+//       next('/error'); // เปลี่ยนเส้นทางไปยังหน้า error
+//       return;
+//     }
+//   }
+//   alert(accessToken.accessToken)
+//   next(); // อนุญาตให้ไปยังเส้นทางที่ร้องขอ
+// });
 
 export default router
