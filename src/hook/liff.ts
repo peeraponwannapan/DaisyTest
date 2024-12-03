f63610f44da0f9a78b3cd64833f69da515dedc60
Manx <manmeisterz@gmail.com>
2024-12-03 21:57:21 +0700

    TEST

--- src/hook/liff.ts
+++ src/hook/liff.ts
@@ -1,4 +1,7 @@
+import { backEndApi } from '@/services/axios';
 import liff, { type Liff } from '@line/liff'
+import { decodeJwt } from 'jose'
+import moment from 'moment'
 
 const useLiff = async (
   liffId = import.meta.env.VITE_LIFF_ID,
@@ -7,13 +10,30 @@ const useLiff = async (
   let liffInstance: Liff | null = null
 
   return liff
-    .init({ liffId: liffId })
-    .then(() => {
+    .init({ liffId })
+    .then(async () => {
       liffInstance = liff
       if (!liff.isLoggedIn()) {
         liff.login({ redirectUri: window.location.href })
       }
+      const getTokenId = liff?.getIDToken() || ''
+      const decode = decodeJwt(getTokenId)
+      const dateToCheck = moment.unix(decode?.exp as number)
+      if (decode && !dateToCheck.isAfter(moment())) {
+        liff.logout();
+        liff.login({ redirectUri: window.location.href });
+      }
+      await backEndApi.post(
+        '/users/login-line',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${getTokenId} `,
+          },
+        },
+      )
       return { liff: liffInstance, error }
+
     })
     .catch(err => {
       // Handle error during initialization
