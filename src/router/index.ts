67740c7d2bd47a4278a87bf96f0abfa8bed2e6a6
Manx <manmeisterz@gmail.com>
2024-12-03 21:54:24 +0700

    TEST

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '@/views/ProductView.vue'
 import OrderStatusView from '@/views/order-status/OrderStatusView.vue'
-import { accessTokenStore } from '@/stores/useAccessTokenLine'
+// import { accessTokenStore } from '@/stores/useAccessTokenLine'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,14 +25,14 @@ const router = createRouter({
   ],
 })
 
-router.beforeEach(async (to, from, next) => {
-  const accessToken = accessTokenStore()  
-  if(!accessToken.accessToken){
-    await accessToken.getIDToken()
-  }
-  console.log("Profile and ID Token fetched successfully");
-  console.log("Access Token:", accessToken.accessToken);
-  next(); // อนุญาตให้ไปยังเส้นทางที่ร้องขอ
-});
+// router.beforeEach(async (to, from, next) => {
+//   const accessToken = accessTokenStore()  
+//   if(!accessToken.accessToken){
+//     await accessToken.getIDToken()
+//   }
+//   console.log("Profile and ID Token fetched successfully");
+//   console.log("Access Token:", accessToken.accessToken);
+//   next(); // อนุญาตให้ไปยังเส้นทางที่ร้องขอ
+// });
 
 export default router
